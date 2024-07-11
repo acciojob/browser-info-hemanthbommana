2327e94f87d87e5cd0ab4630b948b2394032625c
hemanthbommana <117904125+hemanthbommana@users.noreply.github.com>
2024-07-11 13:10:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ function displayMsg(){
 	let browserName = navigator.appName;
 	let version = navigator.appVersion;
 	let ans = document.getElementById('browser-info');
-	ans.innerText("You are using "+browserName+"version"+version);
+	ans.innerText= "You are using "+browserName+"version"+version;
+	
 }
+window.onload = displayBrowserInfo;
